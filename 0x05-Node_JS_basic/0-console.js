7ce6d0d27ec4b1c44d60fe3e0cebf06025691e12
Anoonaa <anoonasithole0@gmail.com>
2024-08-21 01:42:07 +0000

    console , standard input & function for database

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+ * Log a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+// Arrow function
+const displayMessage = (message) => console.log(message);
+
+/**
+ * Export the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
+module.exports = displayMessage;
